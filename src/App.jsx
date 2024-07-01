1887f948bf6ea08e796f0f2ac520893c072ecc41
niluse <niluse02@gmail.com>
2024-07-01 18:57:22 +0300

    init: initial commit -almost done but i forgot to commit-

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+import { useEffect } from "react";
+
+const cardImages = [
+  { src: "../img/helmet-1.png", matched: false },
+  { src: "../img/potion-1.png", matched: false },
+  { src: "../img/ring-1.png", matched: false },
+  { src: "../img/scroll-1.png", matched: false },
+  { src: "../img/shield-1.png", matched: false },
+  { src: "../img/sword-1.png", matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState([0]);
+  const [choiceOne, setchoiceOne] = useState(null);
+  const [choiceTwo, setchoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  //* shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5) //! random dan negatif gelirse karsilastirdigi iki kartin yerini degistirmeyecek eger pozitifse yer degistirecek bu sekilde karismis kar array i else edecegiz
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  //* handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setchoiceTwo(card) : setchoiceOne(card);
+  };
+
+  //* compare 2 selected cards
+  useEffect(() => {
+    setDisabled(true);
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurns();
+      } else {
+        setTimeout(() => resetTurns(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  //* reset choices & increasse turns
+  const resetTurns = () => {
+    setchoiceOne(null);
+    setchoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
